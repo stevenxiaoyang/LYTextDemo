919775e42d568773805ab54f94d628ec694dee64
LuYang <luyangzou@163.com>
2016-06-28 17:49:47 +0800

    Initial Commit

---
+++ LYTextDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYTextDemo
+//
+//  Created by LuYang on 16/6/28.
+//  Copyright © 2016年 LuYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
